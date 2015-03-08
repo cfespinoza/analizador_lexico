274fafd0167f971cd5f9e2299967193aa9db3208
Charles A. Flores Espinoza <cfloresespinoza@gmail.com>
2015-03-08 22:00:19 +0100

    package analizador_lexico_jlex

---
+++ analizador_lexico/src/analizador_lexico_jlex/ClaseLexica.java
@@ -0,0 +1,73 @@
+package analizador_lexico_jlex;
+
+public enum ClaseLexica {
+	
+	SEP, 			// #
+	COMMENT, 		// @
+	SEMICOLON, 		// ;
+	IDEN, 			// (letra|barrabaja)(letra|digito|barrabaja)*
+	TIPO, 			// tipo
+	INT,			// int
+	REAL, 			// real
+	APCOR, 			// [ 
+	CICOR, 			// ]
+	REC, 			// rec
+	ENDREC, 		// endrec
+	POINTER, 		// pointer
+	OBJECT, 		// object
+	ENDOBJECT, 		// endobject
+	EXTENDS, 		// extends
+	END, 			// end
+	FUN, 			// fun
+	METHOD, 		// method
+	RETURN, 		// return
+	AMPERSAN, 		// &
+	THIS, 			// this
+	SUPER, 			// super
+	APPAR, 			// (
+	CIPAR, 			// )
+	DOT, 			// .
+	INVERTEDV, 		// ^
+	GREATERTHAN, 	// >
+	LOWERTHAN, 		// <
+	GREATEREQ, 		// >=
+	LOWEREQ, 		// <=
+	EQUAL, 			// ==
+	DISTINCT, 		// !=
+	OR, 			// \|\|
+	PLUS, 			// +
+	MINUS, 			// -
+	MULTI, 			// \*
+	DIVISION, 		// /
+	MOD, 			// %
+	AND, 			// &&
+	NEGATION, 		// !
+	NULL, 			// null	
+	ASSIG, 			// =
+	IN, 			// in
+	OUT, 			// out
+	ALLOC, 			// alloc
+	FREE, 			// free
+	IF, 			// if
+	ELSE, 			// else
+	FI, 			// fi
+	ELSEIF, 		// elseif
+	THEN, 			// then
+	WHILE, 			// while
+	DO, 			// do
+	ENDWHILE, 		// endwhile
+	ARROBA,			// @
+	NUMBER,			// NUMENTERO | NUMREAL
+	
+	// AUXILIARES
+	
+	NUMENTERO,	 	// POSDIGIT CERO
+	NUMREAL,		// 
+	CERO, 			// 0
+	UNDERSCORE,		// _
+	POSDIGIT,		// 1..9
+	EOF, 			// EOF
+	LETTER			// (a..z|A..Z)
+
+}
+
